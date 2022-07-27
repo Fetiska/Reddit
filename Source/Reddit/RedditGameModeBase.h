94bb5fdc8ba4be06e2848b445021efa74e6eebc5
Fetiska <80163382+Fetiska@users.noreply.github.com>
2022-07-27 20:18:43 +0300

    Initial

---
+++ Source/Reddit/RedditGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedditGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDDIT_API ARedditGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
